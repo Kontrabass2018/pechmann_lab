a65eb141574d857597d88f3e9464ed5c54b2ddac
Léonard Sauvé <leonardsauve@vpn-10-39-1-107.acd.umontreal.ca>
2018-03-20 03:07:58 -0400

    some changes

--- compute_contacts.py
+++ compute_contacts.py
@@ -26,27 +26,43 @@ def runDSSP(filename):
 def parseDSSP(DSSPresult):
 	logging.info('# in result? -- > ', "#" in DSSPresult)
 	header_start = DSSPresult.index('#')
+	ids = []
+	res_num = []
+	amino_acids = []
+	accessibility = []
+
 	for line in DSSPresult[header_start:].split("\n"):
-		print line
-		# id, res, aa, acc = line[4:6], line[9:11], line[14:15], line[35:38]
-		
-	#DSSPhandle = StringIO(DSSPresult[header_start:])
-	#DSSPheader = DSSPhandle.readline().strip().split()
-	#DSSPbody = [line.strip().split() for line in list(DSSPhandle.readlines())]
-	##DSSPdf = pd.read_csv(DSSPhandle, sep = '\\s')  	
+		id, res, aa, acc = line[4:6], line[9:11], line[14:15], line[35:38]
+		ids.append(id)
+		res_num.append(res)
+		amino_acids.append(aa)
+		accessibility.append(acc)
+	
+	DSSPdf = pd.DataFrame(list(zip(ids, res_num, amino_acids, accessibility)),
+              columns=['id','res_num', 'aa', 'acc'])
+
 	return DSSPdf
 
+def color_pdb(DSSPdf, pdb_file):
+
+	core_res = df.loc[DSSPdf.acc <= treshold]
+
+	logging.info(core_res.head())
 if __name__ == '__main__': 
 	parser =  argparse.ArgumentParser(
 		description = 'this is a program that annotates and describes physical properties of a protein structure.')
 
 	parser.add_argument('--single_input', type = str, help  = 'name of input file path in pdb format')
 
-	parser.add_argument('--dssp', action = 'store_false', help = 'prevent prgm from using dssp information to compute surface area information.')
+	parser.add_argument('--no_dssp', action = 'store_false', help = 'prevent prgm from using dssp information to compute surface area information.')
 
-	parser.add_argument('--hydroph_contacts', action = 'store_false', help = 'prevent prgm from using hydrophobic contacts for the given protein')
+	parser.add_argument('--no_hydroph_contacts', action = 'store_false', help = 'prevent prgm from using hydrophobic contacts for the given protein')
 
 	parser.add_argument('--outfile', type=str , help = 'name of output file')
+
+	parser.add_argument('--treshold', nargs = '?', default = 50, help = 'solvent accessible treshold to decide if residue is in core or not. Under or equal to treshhold means burried in core')
+
+	parser.add_argument('--color_structures', action = 'store_true', help = 'colors the pdb structure according to acc profile and saves png')
 
 	parser.add_argument('--debug', action= 'store_true', help = 'print debug data')
 
@@ -63,6 +79,21 @@ if __name__ == '__main__':
 
 	logging.info(parsedDSSP.head())
 
-	print list(parsedDSSP)
-
 	logging.info('colnames: ', str(list(parsedDSSP)))	
+
+	if color_structures:
+
+		pdb_file = args.single_input
+		
+		color_pdb(parseDSSP, pdb_file, args.treshold)
+
+	parsedDSSP.to_csv(args.outfile, sep = "\t")
+
+
+
+
+
+
+
+
+
